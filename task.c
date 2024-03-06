779bcd71485ae6618d38a660e91b3b5ab8edc76a
Yann Adjumany <69991917+A-yann-axel-V@users.noreply.github.com>
2024-03-06 05:21:46 +0100

    updated

--- task.c
+++ task.c
@@ -8,12 +8,12 @@
  */
 size_t custom_strlen(const char *str)
 {
-    size_t length = 0;
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    return length;
+        size_t length = 0;
+        while (str[length] != '\0')
+        {
+                length++;
+        }
+        return length;
 }
 
 /**
@@ -22,34 +22,34 @@ size_t custom_strlen(const char *str)
  */
 void execute_command(char *buffer)
 {
-    char *token;
-    pid_t pid;
-    int status;
+        char *token;
+        pid_t pid;
+        int status;
 
-    token = strtok(buffer, " ");
-    if (token != NULL)
-    {
-        pid = fork();
-        if (pid < 0)
+        token = strtok(buffer, " ");
+        if (token != NULL)
         {
-            perror("fork");
-            exit(EXIT_FAILURE);
+                pid = fork();
+                if (pid < 0)
+                {
+                        perror("fork");
+                        exit(EXIT_FAILURE);
+                }
+                else if (pid == 0)
+                {
+                        /* Child process */
+                        if (execve(token, &token, NULL) == -1)
+                        {
+                                perror("execve");
+                                exit(EXIT_FAILURE);
+                        }
+                }
+                else
+                {
+                        /* Parent process */
+                        waitpid(pid, &status, 0);
+                }
         }
-        else if (pid == 0)
-        {
-            /* Child process */
-            if (execve(token, &token, NULL) == -1)
-            {
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process */
-            waitpid(pid, &status, 0);
-        }
-    }
 }
 
 /**
@@ -59,35 +59,34 @@ void execute_command(char *buffer)
  */
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    ssize_t characters_read;
+        char *buffer = NULL;
+        size_t bufsize = 0;
+        ssize_t characters_read;
 
-    while (1)
-    {
-        printf("$ ");
-        fflush(stdout);
-
-        characters_read = getline(&buffer, &bufsize, stdin);
-        if (characters_read == -1)
+        while (1)
         {
-            if (feof(stdin))
-            {
-                printf("\n");
-                break;
-            }
-            else
-            {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
+                printf("$ ");
+                fflush(stdout);
+
+                characters_read = getline(&buffer, &bufsize, stdin);
+                if (characters_read == -1)
+                {
+                        if (feof(stdin))
+                        {
+                                printf("\n");
+                                break;
+                        }
+                        else
+                        {
+                                perror("getline");
+                                exit(EXIT_FAILURE);
+                        }
+                }
+
+                buffer[custom_strlen(buffer) - 1] = '\0'; /* Remove newline character */
+                execute_command(buffer);
         }
 
-        buffer[custom_strlen(buffer) - 1] = '\0'; /* Remove newline character */
-
-        execute_command(buffer);
-    }
-
-    free(buffer);
-    return 0;
+        free(buffer);
+        return 0;
 }
