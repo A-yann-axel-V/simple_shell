db9f427beb9d0f9b039fb3209b4085cb3b3aee78
Yann Adjumany <yvay03@gmail.com>
2024-03-06 04:35:29 +0000

    betty checked

--- task.c
+++ task.c
@@ -6,34 +6,34 @@
  */
 void execute_command(char *buffer)
 {
-        char *token;
-        pid_t pid;
-        int status;
+	char *token;
+	pid_t pid;
+	int status;
 
-        token = strtok(buffer, " ");
-        if (token != NULL)
-        {
-                pid = fork();
-                if (pid < 0)
-                {
-                        perror("fork");
-                        exit(EXIT_FAILURE);
-                }
-                else if (pid == 0)
-                {
-                        /* Child process */
-                        if (execve(token, &token, NULL) == -1)
-                        {
-                                perror("execve");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-                else
-                {
-                        /* Parent process */
-                        waitpid(pid, &status, 0);
-                }
-        }
+	token = strtok(buffer, " ");
+	if (token != NULL)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			if (execve(token, &token, NULL) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			waitpid(pid, &status, 0);
+		}
+	}
 }
 
 /**
@@ -43,34 +43,34 @@ void execute_command(char *buffer)
  */
 int main(void)
 {
-        char *buffer = NULL;
-        size_t bufsize = 0;
-        ssize_t characters_read;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t characters_read;
 
-        while (1)
-        {
-                printf("$ ");
-                fflush(stdout);
+	while (1)
+	{
+		printf("$ ");
+		fflush(stdout);
 
-                characters_read = getline(&buffer, &bufsize, stdin);
-                if (characters_read == -1)
-                {
-                        if (feof(stdin))
-                        {
-                                printf("\n");
-                                break;
-                        }
-                        else
-                        {
-                                perror("getline");
-                                exit(EXIT_FAILURE);
-                        }
-                }
+		characters_read = getline(&buffer, &bufsize, stdin);
+		if (characters_read == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-                buffer[str_len(buffer) - 1] = '\0'; /* Remove newline character */
-                execute_command(buffer);
-        }
+		buffer[str_len(buffer) - 1] = '\0'; /* Remove newline character */
+		execute_command(buffer);
+	}
 
-        free(buffer);
-        return (0);
+	free(buffer);
+	return (0);
 }
