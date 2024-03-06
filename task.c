4d3de2eb3d6b981f0971c41ede3be7699f4ffaee
Yann Adjumany <yvay03@gmail.com>
2024-03-06 04:47:15 +0000

    checked

--- task.c
+++ task.c
@@ -67,10 +67,71 @@ int main(void)
 			}
 		}
 
-		buffer[str_len(buffer) - 1] = '\0'; /* Remove newline character */
+		buffer[_strlen(buffer) - 1] = '\0'; /* Remove newline character */
 		execute_command(buffer);
 	}
 
 	free(buffer);
 	return (0);
 }
+
+
+/**
+ * get_sigint - Handle the crtl + c call in prompt
+ * @sig: Signal handler
+ *
+ * Return: void
+ */
+void get_sigint(int sig)
+{
+	if (sig == SIGINT)
+		write(STDOUT_FILENO, "\n\\_(^-^)_/\n", 12);
+}
+
+/**
+ * execute - executes command
+ * @command: command to execute
+ *
+ * Return: 0 for success, -1 for failure
+ */
+int execute(data_shell command)
+{
+	int status, found = 0;
+	char *exec = NULL;
+
+	if (access(command.args[0], X_OK) == 0)
+		found = 1;
+	else
+	{
+		exec = find_executable(command.args[0], command._environ);
+		if (exec)
+		{
+			found = 1;
+			command.args[0] = _strdup(exec);
+		}
+		else
+			command.status = 2;
+		free(exec);
+	}
+	if (found == 1)
+	{
+		command.pid = fork(); /* Fork a new process */
+		if (command.pid == 0)
+		{
+			command.status = 0;
+			return (execve(command.args[0], command.args, command._environ));
+		}
+		else if (command.pid > 0) /* Wait for the child process to complete */
+		{
+			waitpid(command.pid, &status, 0);
+			command.status = WEXITSTATUS(status);
+		}
+		else
+			perror("./hsh: 0");
+	}
+	else
+		command.status = 127;
+	get_error(command.args, command.status, command.counter);
+	return (0);
+}
+
